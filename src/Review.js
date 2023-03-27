ad471d33fd4fcb6b6b5314a7057879f6f0658ddc
Giacomo De Pol <giacomo.depol@hotmail.it>
2023-03-27 12:47:24 +0200

    trying to add prev and next button

--- src/Review.js
+++ src/Review.js
@@ -2,9 +2,23 @@ import React, { useState } from "react";
 import people from "./data";
 import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
+
 const Review = () => {
   const [index, setIndex] = useState(0);
   const { name, job, image, text } = people[index];
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      return newIndex;
+    });
+  };
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      return newIndex;
+  });
+
   return (
     <article className="review">
       <div className="img-container">
@@ -17,10 +31,10 @@ const Review = () => {
       <p className="job">{job}</p>
       <p className="info">{text}</p>
       <div className="button-container">
-        <button className="prev-btn">
+        <button className="prev-btn" onClick={prevPerson}>
           <FaChevronLeft />
         </button>
-        <button className="next-btn">
+        <button className="next-btn" onClick={nextPerson}>
           <FaChevronRight />
         </button>
       </div>
